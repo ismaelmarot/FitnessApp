527e255e3fc33890763c557d4a6a219451061495
ismaelmarot <43220852+ismaelmarot@users.noreply.github.com>
2025-12-05 21:03:33 -0300

    prisma

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,10 +5,7 @@ const app = express();
 app.use(express.json());
 
 app.get('/users', async (req, res) => {
-  const users = await prisma.user.findMany();
-  res.json(users);
+  res.json(await prisma.user.findMany());
 });
 
-app.listen(3001, () => {
-  console.log('Backend running on http://localhost:3001');
-});
+app.listen(3001, () => console.log("Server running"));
